78cfa7d5b9a4f68f76b4985a5fb709e986828d4d
Alex Alves <jhosealex@gmail.com>
2021-03-24 18:09:56 -0300

    feat: Método de setar velocidade do trem e verificação para não andar

--- trem.cpp
+++ trem.cpp
@@ -13,9 +13,20 @@ Trem::Trem(int ID, int x, int y){
 void Trem::run(){
     while(true){
         switch(ID){
-        case 1:     //Trem 1
-            velocidade = 50;
-            if (y == 30 && x <330)
+        case 1:     //Trem 1s
+//            if(x == 310 && y == 30){
+//                printf("Bloqueado pelo trem 1\n");
+//                pthread_mutex_lock(&mutex);
+//            }
+//            if(x == 330 && y == 150){
+//                printf("Bloqueado pelo trem 1\n");
+//                pthread_mutex_unlock(&mutex);
+//            }
+            if(velocidade == 0){
+                emit updateGUI(ID, x,y);
+                break;
+            }
+            else if (y == 30 && x <330)
                 x+=10;
             else if (x == 330 && y < 150)
                 y+=10;
@@ -26,7 +37,19 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 2: //Trem 2
-            if (y == 30 && x <600)
+//            if(x == 350 && y == 150){
+//                printf("Bloqueado pelo trem 2\n");
+//                pthread_mutex_lock(&mutex);
+//            }
+//            if(x == 330 && y == 30){
+//                printf("Bloqueado pelo trem 2\n");
+//                pthread_mutex_unlock(&mutex);
+//            }
+            if(velocidade == 0){
+                emit updateGUI(ID, x,y);
+                break;
+            }
+            else if (y == 30 && x <600)
                 x+=10;
             else if (x == 600 && y < 150)
                 y+=10;
@@ -37,7 +60,11 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 3: //Trem 3
-            if (y == 30 && x <870)
+            if(velocidade == 0){
+                emit updateGUI(ID, x,y);
+                break;
+            }
+            else if (y == 30 && x <870)
                 x+=10;
             else if (x == 870 && y < 150)
                 y+=10;
@@ -48,7 +75,11 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 4: //Trem 4
-            if (y == 150 && x <470)
+            if(velocidade == 0){
+                emit updateGUI(ID, x,y);
+                break;
+            }
+            else if (y == 150 && x <470)
                 x+=10;
             else if (x == 470 && y < 270)
                 y+=10;
@@ -59,7 +90,11 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 5: //Trem 5
-            if (y == 150 && x <740)
+            if(velocidade == 0){
+                emit updateGUI(ID, x,y);
+                break;
+            }
+            else if (y == 150 && x <740)
                 x+=10;
             else if (x == 740 && y < 270)
                 y+=10;
@@ -77,6 +112,13 @@ void Trem::run(){
     }
 }
 
-
+// Setar velocidade
+void Trem::setVelocidade(int v){
+    if(v <= 0){
+        velocidade = 0;
+        return;
+    }
+    velocidade = 99*7/v;
+}
 
 
